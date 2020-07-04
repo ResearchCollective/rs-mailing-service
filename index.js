0d3c04106c61ac982bb4cf677d497237f439eacd
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-07-04 11:54:50 -0400

    working version, Note:- Can use some polishing

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 const express = require('express')
+const sendpulse = require('sendpulse-api')
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
+require('dotenv').config()
 
 // initialing app//
 const app = express()
+
+// parse various data//
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
 // greetining//
 app.get('/', (req, res, next) => {
     res.json('This is mailing microservice for researchcollective.io')
+    next()
+})
+
+app.post('/', async (req, res) => {
+    const {fullName, email, message} = req.body
+
+    // Note to self:- Do not push code with SMTP config, put them in enviroment variable//
+    let transporter = nodemailer.createTransport({
+        host: 'smtp-pulse.com',//transporter host name
+        port: 465,//transporter port
+        secure: true, //true for port 465, false for other ports
+        auth: {
+            user: process.env.SENDPULSE_ACCOUNT_EMAIL, // sendpulse account email
+            pass: process.env.SENDPULSE_ACCOUNT_PASSWORD, // sendpulse account password
+          },
+        //this is just for testing on localhost //
+          tls: {
+            rejectUnAuthorized: false
+          }
+    })
+    // send mail with transporter//
+    let mail = await transporter.sendMail({
+        from: `"Research Collective Contact" <${process.env.TRANSPORTER_EMAIL}>`, 
+        to: process.env.TO_EMAIL, // can put multiple accounts//
+        subject: 'Research Colletive Contact Request',
+        // TODO: setting up some email template (maybe)
+        text: message, //plain text body//
+        html: message//object from user's message
+    })
+    // Message sent//
+    console.log("Message sent: %s", mail.messageId);
+    // res.json("Message sent: %s", mail.messageId);
+    // Preview only available when sending through an Ethereal account
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(mail));
+    return res.status(200).json('Email sent succesfully')
 })
 
 //setting up the port//
 app.listen(8000, () => {
-    console.log('Microservice is now active')
+    console.log('Mailing microservice is now active')
 })
